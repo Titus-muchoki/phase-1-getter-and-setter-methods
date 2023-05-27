53dcc50fc7efdf7f2327b8e1cd7b1f267b5251b9
kajela <titoyut56@gmail.com>
2023-05-27 03:30:39 -0400

    add the initial files

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your Circle class here
+class Circle {
+
+    constructor(radius){
+        this.radius = radius;
+        // circle = new Circle(6);
+
+    }
+    get diameter(){
+      this.radius =  Math.PI(this.radius)
+    }
+    get circumference(){
+
+    }
+    get area(){
+
+    }
+    set diameter(newDiameter){
+        this.diameter = diameter;
+    }
+    set circumference(newCircumference){
+        this.circumference = circumference;
+    }
+    set area(newArea){
+        this.area =  Math.sqrt(newArea);
+    }
+}
